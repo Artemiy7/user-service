aefefae9cbd2d0f0d8b308589e96d42da199ec11
a1 <Artemiy7>
2023-06-23 09:47:53 +0300

    Create unit tests #25

---
+++ src/test/java/com/mongotask/api/MongoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongotask.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class MongoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
