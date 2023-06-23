34e69869fff1139943ac834c9fe565884bc172ed
a1 <Artemiy7>
2023-06-23 09:33:05 +0300

    Create Error response #6

---
+++ src/main/java/com/mongotask/api/response/error/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.mongotask.api.response.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorResponse {
+    private HttpStatus httpStatus;
+    private String path;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime localDateTime;
+    private String message;
+}
