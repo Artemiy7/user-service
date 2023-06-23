0bd96dce09f9da20705bebde8e0b056043494578
a1 <Artemiy7>
2023-06-23 09:32:09 +0300

    Create SequenceGeneratorService class #4

---
+++ src/main/java/com/mongotask/api/services/SequenceGeneratorService.java
@@ -0,0 +1,27 @@
+package com.mongotask.api.services;
+
+
+import com.mongotask.api.domain.MongoDBSequence;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+@RequiredArgsConstructor
+@Service
+public class SequenceGeneratorService {
+    private final MongoOperations mongoOperations;
+
+    public String generateSequence(String seqName) {
+        MongoDBSequence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
+                new Update().inc("seq", 1), options().returnNew(true).upsert(true),
+                MongoDBSequence.class);
+        return !Objects.isNull(counter) ? counter.getSeq() : "1";
+    }
+}
